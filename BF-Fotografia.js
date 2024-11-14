9d61a5d1bbb335d4aedfd9261e4a0f6e52488661
BFbraian2023 <158025437+BFbraian2023@users.noreply.github.com>
2024-11-14 11:59:46 -0300

    Add files via upload

---
+++ BF-Fotografia.js
@@ -0,0 +1,88 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+  
+  function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+}
+
+const carrusel = document.querySelector(".carrusel-items");
+
+let maxScrollLeft = carrusel.scrollWidth - carrusel.clientWidth;
+let intervalo = null;
+let step = 1;
+const start = () => {
+  intervalo = setInterval(function () {
+    carrusel.scrollLeft = carrusel.scrollLeft + step;
+    if (carrusel.scrollLeft === maxScrollLeft) {
+      step = step * -1;
+    } else if (carrusel.scrollLeft === 0) {
+      step = step * -1;
+    }
+  }, 10);
+};
+
+const stop = () => {
+  clearInterval(intervalo);
+};
+
+carrusel.addEventListener("mouseover", () => {
+  stop();
+});
+
+carrusel.addEventListener("mouseout", () => {
+  start();
+});
+
+start();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
